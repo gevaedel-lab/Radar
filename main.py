b353d139b0458cdd058bac00c37ad6d020b8ebb6
gevaedel-lab <geva.edel@gmail.com>
2026-03-10 11:24:15 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+זווית_רדאר = 0
+זמן_השהיה = 0
 רדאר_פועל = 0
+מרחק = 0
+מרחק_גילוי_מקסימאלי = 0
+זווית_סריקה_מקסימאלית = 0
 
 def on_button_pressed_a():
-    global רדאר_פועל
+    global זווית_רדאר, זמן_השהיה, רדאר_פועל
+    זווית_רדאר = 0
+    זמן_השהיה = 100
+    SuperBit.servo2(SuperBit.enServo.S3, זווית_רדאר)
+    basic.pause(1000)
     רדאר_פועל = 1
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
@@ -11,8 +20,36 @@ def on_button_pressed_b():
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_forever():
-    while רדאר_פועל == 1:
-        music.play(music.tone_playable(1000 - 6 * sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.CENTIMETERS),
-                music.beat(BeatFraction.SIXTEENTH)),
+    if מרחק < מרחק_גילוי_מקסימאלי:
+        music.play(music.create_sound_expression(WaveShape.SINE,
+                5000,
+                1,
+                255,
+                255,
+                500,
+                SoundExpressionEffect.VIBRATO,
+                InterpolationCurve.LINEAR),
             music.PlaybackMode.UNTIL_DONE)
 basic.forever(on_forever)
+
+def on_forever2():
+    global מרחק, זווית_סריקה_מקסימאלית, מרחק_גילוי_מקסימאלי, זווית_רדאר
+    מרחק = 1000
+    זווית_סריקה_מקסימאלית = 90
+    מרחק_גילוי_מקסימאלי = 100
+    while רדאר_פועל == 1:
+        while זווית_רדאר < זווית_סריקה_מקסימאלית:
+            זווית_רדאר = זווית_רדאר + 10
+            SuperBit.servo2(SuperBit.enServo.S3, זווית_רדאר)
+            מרחק = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.CENTIMETERS)
+            music.play(music.tone_playable(1000 - 6 * מרחק, music.beat(BeatFraction.SIXTEENTH)),
+                music.PlaybackMode.UNTIL_DONE)
+            basic.pause(זמן_השהיה)
+        while זווית_רדאר > 0:
+            זווית_רדאר = זווית_רדאר - 10
+            SuperBit.servo2(SuperBit.enServo.S3, זווית_רדאר)
+            מרחק = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.CENTIMETERS)
+            music.play(music.tone_playable(1000 - 6 * מרחק, music.beat(BeatFraction.SIXTEENTH)),
+                music.PlaybackMode.UNTIL_DONE)
+            basic.pause(זמן_השהיה)
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -4,22 +4,26 @@ let רדאר_פועל = 0
 let מרחק = 0
 let מרחק_גילוי_מקסימאלי = 0
 let זווית_סריקה_מקסימאלית = 0
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
     זווית_רדאר = 0
     זמן_השהיה = 100
     SuperBit.Servo2(SuperBit.enServo.S3, זווית_רדאר)
     basic.pause(1000)
     רדאר_פועל = 1
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
     רדאר_פועל = 0
 })
-basic.forever(function () {
+basic.forever(function on_forever() {
     if (מרחק < מרחק_גילוי_מקסימאלי) {
         music.play(music.createSoundExpression(WaveShape.Sine, 5000, 1, 255, 255, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     }
+    
 })
-basic.forever(function () {
+basic.forever(function on_forever2() {
+    
     מרחק = 1000
     זווית_סריקה_מקסימאלית = 90
     מרחק_גילוי_מקסימאלי = 100
@@ -27,22 +31,14 @@ basic.forever(function () {
         while (זווית_רדאר < זווית_סריקה_מקסימאלית) {
             זווית_רדאר = זווית_רדאר + 10
             SuperBit.Servo2(SuperBit.enServo.S3, זווית_רדאר)
-            מרחק = sonar.ping(
-            DigitalPin.P1,
-            DigitalPin.P2,
-            PingUnit.Centimeters
-            )
+            מרחק = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
             music.play(music.tonePlayable(1000 - 6 * מרחק, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
             basic.pause(זמן_השהיה)
         }
         while (זווית_רדאר > 0) {
             זווית_רדאר = זווית_רדאר - 10
             SuperBit.Servo2(SuperBit.enServo.S3, זווית_רדאר)
-            מרחק = sonar.ping(
-            DigitalPin.P1,
-            DigitalPin.P2,
-            PingUnit.Centimeters
-            )
+            מרחק = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
             music.play(music.tonePlayable(1000 - 6 * מרחק, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
             basic.pause(זמן_השהיה)
         }
