4ac89c7b4c2404783404547d5cd57c1815a07116
gevaedel-lab <geva.edel@gmail.com>
2026-03-15 17:06:25 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 זמן_השהיה = 0
 רדאר_פועל = 0
 מרחק = 0
-מרחק_גילוי_מקסימאלי = 0
 זווית_סריקה_מקסימאלית = 0
+מרחק_גילוי_מקסימאלי = 0
 
 def on_button_pressed_a():
     global זווית_רדאר, זמן_השהיה, רדאר_פועל
     זווית_רדאר = 0
-    זמן_השהיה = 100
+    זמן_השהיה = 200
     SuperBit.servo2(SuperBit.enServo.S3, זווית_רדאר)
     basic.pause(1000)
     רדאר_פועל = 1
@@ -20,19 +20,6 @@ def on_button_pressed_b():
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_forever():
-    if מרחק < מרחק_גילוי_מקסימאלי:
-        music.play(music.create_sound_expression(WaveShape.SINE,
-                5000,
-                1,
-                255,
-                255,
-                500,
-                SoundExpressionEffect.VIBRATO,
-                InterpolationCurve.LINEAR),
-            music.PlaybackMode.UNTIL_DONE)
-basic.forever(on_forever)
-
-def on_forever2():
     global מרחק, זווית_סריקה_מקסימאלית, מרחק_גילוי_מקסימאלי, זווית_רדאר
     מרחק = 1000
     זווית_סריקה_מקסימאלית = 90
@@ -52,4 +39,17 @@ def on_forever2():
             music.play(music.tone_playable(1000 - 6 * מרחק, music.beat(BeatFraction.SIXTEENTH)),
                 music.PlaybackMode.UNTIL_DONE)
             basic.pause(זמן_השהיה)
+basic.forever(on_forever)
+
+def on_forever2():
+    if מרחק < מרחק_גילוי_מקסימאלי:
+        music.play(music.create_sound_expression(WaveShape.SINE,
+                5000,
+                1,
+                255,
+                255,
+                500,
+                SoundExpressionEffect.VIBRATO,
+                InterpolationCurve.LINEAR),
+            music.PlaybackMode.UNTIL_DONE)
 basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,29 +1,30 @@
 let זווית_רדאר = 0
 let זמן_השהיה = 0
 let רדאר_פועל = 0
+let זווית_תותח = 0
 let מרחק = 0
-let מרחק_גילוי_מקסימאלי = 0
 let זווית_סריקה_מקסימאלית = 0
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+let מרחק_גילוי_מקסימאלי = 0
+input.onButtonPressed(Button.A, function () {
     זווית_רדאר = 0
-    זמן_השהיה = 100
+    זמן_השהיה = 200
     SuperBit.Servo2(SuperBit.enServo.S3, זווית_רדאר)
     basic.pause(1000)
     רדאר_פועל = 1
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
+function חשב_זווית_תותח (זווית_רדאר: number) {
+    זווית_תותח = 270 - זווית_רדאר * 2
+    זווית_תותח = זווית_תותח - 90
+}
+input.onButtonPressed(Button.AB, function () {
     רדאר_פועל = 0
 })
-basic.forever(function on_forever() {
-    if (מרחק < מרחק_גילוי_מקסימאלי) {
-        music.play(music.createSoundExpression(WaveShape.Sine, 5000, 1, 255, 255, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
-    }
-    
+input.onButtonPressed(Button.B, function () {
+    SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
+    basic.pause(500)
+    SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
 })
-basic.forever(function on_forever2() {
-    
+basic.forever(function () {
     מרחק = 1000
     זווית_סריקה_מקסימאלית = 90
     מרחק_גילוי_מקסימאלי = 100
@@ -31,16 +32,34 @@ basic.forever(function on_forever2() {
         while (זווית_רדאר < זווית_סריקה_מקסימאלית) {
             זווית_רדאר = זווית_רדאר + 10
             SuperBit.Servo2(SuperBit.enServo.S3, זווית_רדאר)
-            מרחק = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
+            מרחק = sonar.ping(
+            DigitalPin.P1,
+            DigitalPin.P2,
+            PingUnit.Centimeters
+            )
             music.play(music.tonePlayable(1000 - 6 * מרחק, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
             basic.pause(זמן_השהיה)
         }
         while (זווית_רדאר > 0) {
             זווית_רדאר = זווית_רדאר - 10
             SuperBit.Servo2(SuperBit.enServo.S3, זווית_רדאר)
-            מרחק = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
+            מרחק = sonar.ping(
+            DigitalPin.P1,
+            DigitalPin.P2,
+            PingUnit.Centimeters
+            )
             music.play(music.tonePlayable(1000 - 6 * מרחק, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
             basic.pause(זמן_השהיה)
         }
+    }
+})
+basic.forever(function () {
+    if (מרחק < מרחק_גילוי_מקסימאלי) {
+        music.play(music.createSoundExpression(WaveShape.Sine, 5000, 1, 255, 255, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+        חשב_זווית_תותח(זווית_רדאר)
+        SuperBit.Servo2(SuperBit.enServo.S5, זווית_תותח)
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
+        basic.pause(500)
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
     }
 })
