235d6bada372e74cfdb8dd607eefcea6bfc19c1b
gevaedel-lab <geva.edel@gmail.com>
2026-03-15 17:44:19 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ let מרחק = 0
 let זווית_סריקה_מקסימאלית = 0
 let מרחק_גילוי_מקסימאלי = 0
 input.onButtonPressed(Button.A, function () {
-    זווית_רדאר = 0
+    זווית_רדאר = 45
     זמן_השהיה = 200
     SuperBit.Servo2(SuperBit.enServo.S3, זווית_רדאר)
     basic.pause(1000)
@@ -17,12 +17,16 @@ function חשב_זווית_תותח (זווית_רדאר: number) {
     זווית_תותח = זווית_תותח - 90
 }
 input.onButtonPressed(Button.AB, function () {
-    רדאר_פועל = 0
+	
 })
 input.onButtonPressed(Button.B, function () {
-    SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
-    basic.pause(500)
-    SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
+    if (רדאר_פועל == 0) {
+        SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
+        basic.pause(1000)
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
+    } else if (רדאר_פועל == 1) {
+        רדאר_פועל = 0
+    }
 })
 basic.forever(function () {
     מרחק = 1000
@@ -59,7 +63,7 @@ basic.forever(function () {
         חשב_זווית_תותח(זווית_רדאר)
         SuperBit.Servo2(SuperBit.enServo.S5, זווית_תותח)
         SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
-        basic.pause(500)
+        basic.pause(1000)
         SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
     }
 })
