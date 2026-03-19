4fb9c18f5a5f81cd1aa76ea24234329ae21738de
gevaedel-lab <geva.edel@gmail.com>
2026-03-19 17:04:37 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,55 +1,196 @@
-זווית_רדאר = 0
-זמן_השהיה = 0
-רדאר_פועל = 0
-מרחק = 0
-זווית_סריקה_מקסימאלית = 0
-מרחק_גילוי_מקסימאלי = 0
+def on_gesture_tilt_right():
+    radio.send_value("כיוון", 1)
+    basic.show_leds("""
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        """)
+    music.play(music.string_playable("F - - - F - - - ", 200),
+        music.PlaybackMode.UNTIL_DONE)
+input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
 
 def on_button_pressed_a():
-    global זווית_רדאר, זמן_השהיה, רדאר_פועל
-    זווית_רדאר = 0
-    זמן_השהיה = 200
-    SuperBit.servo2(SuperBit.enServo.S3, זווית_רדאר)
-    basic.pause(1000)
-    רדאר_פועל = 1
+    radio.send_value("כפתור", 1)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def חשב_זווית_תותח(זווית_רדאר: number):
+    global זווית_תותח
+    זווית_תותח = 270 - זווית_רדאר * 2
+    זווית_תותח = זווית_תותח - 90
+
+def on_gesture_tilt_left():
+    radio.send_value("כיוון", -1)
+    basic.show_leds("""
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        """)
+    music.play(music.string_playable("C5 - - - C5 - - - ", 200),
+        music.PlaybackMode.UNTIL_DONE)
+input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
+
+def on_button_pressed_ab():
+    global הפעלת_נסיעה
+    if הפעלת_נסיעה == 0:
+        הפעלת_נסיעה = 1
+        radio.send_value("מהירות", -255)
+        basic.show_leds("""
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            """)
+    elif הפעלת_נסיעה == 1:
+        הפעלת_נסיעה = 0
+        basic.show_leds("""
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            """)
+        radio.send_value("מהירות", 0)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
 def on_button_pressed_b():
-    global רדאר_פועל
-    רדאר_פועל = 0
+    radio.send_value("כפתור", 2)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+def on_received_value(name, value):
+    global זווית_רדאר2, זמן_השהיה, רדאר_פועל, מהירות, כיוון
+    if 1 == value and "כפתור" == name:
+        זווית_רדאר2 = 55
+        זמן_השהיה = 200
+        SuperBit.servo2(SuperBit.enServo.S3, זווית_רדאר2)
+        basic.pause(1000)
+        רדאר_פועל = 1
+    elif 2 == value and "כפתור" == name:
+        if רדאר_פועל == 0:
+            SuperBit.motor_run(SuperBit.enMotors.M1, -255)
+            basic.pause(1000)
+            SuperBit.motor_run(SuperBit.enMotors.M1, 0)
+        elif רדאר_פועל == 1:
+            רדאר_פועל = 0
+    elif "מהירות" == name:
+        מהירות = value
+        כיוון = 0
+    elif "כיוון" == name:
+        כיוון = value
+radio.on_received_value(on_received_value)
+
+מרחק_גילוי_מקסימאלי = 0
+זווית_סריקה_מקסימאלית = 0
+מרחק = 0
+קול = 0
+כיוון = 0
+מהירות = 0
+רדאר_פועל = 0
+זמן_השהיה = 0
+זווית_רדאר2 = 0
+זווית_תותח = 0
+הפעלת_נסיעה = 0
+radio.set_group(108)
+הפעלת_נסיעה = 0
+
 def on_forever():
-    global מרחק, זווית_סריקה_מקסימאלית, מרחק_גילוי_מקסימאלי, זווית_רדאר
+    global קול, מרחק, זווית_סריקה_מקסימאלית, מרחק_גילוי_מקסימאלי, זווית_רדאר2
+    קול = 0
     מרחק = 1000
-    זווית_סריקה_מקסימאלית = 90
+    זווית_סריקה_מקסימאלית = 110
     מרחק_גילוי_מקסימאלי = 100
     while רדאר_פועל == 1:
-        while זווית_רדאר < זווית_סריקה_מקסימאלית:
-            זווית_רדאר = זווית_רדאר + 10
-            SuperBit.servo2(SuperBit.enServo.S3, זווית_רדאר)
+        while זווית_רדאר2 < זווית_סריקה_מקסימאלית:
+            זווית_רדאר2 = זווית_רדאר2 + 10
+            SuperBit.servo2(SuperBit.enServo.S3, זווית_רדאר2)
             מרחק = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.CENTIMETERS)
-            music.play(music.tone_playable(1000 - 6 * מרחק, music.beat(BeatFraction.SIXTEENTH)),
-                music.PlaybackMode.UNTIL_DONE)
+            if קול == 1:
+                music.play(music.tone_playable(1000 - 6 * מרחק, music.beat(BeatFraction.SIXTEENTH)),
+                    music.PlaybackMode.UNTIL_DONE)
             basic.pause(זמן_השהיה)
-        while זווית_רדאר > 0:
-            זווית_רדאר = זווית_רדאר - 10
-            SuperBit.servo2(SuperBit.enServo.S3, זווית_רדאר)
+        while זווית_רדאר2 > 0:
+            זווית_רדאר2 = זווית_רדאר2 - 10
+            SuperBit.servo2(SuperBit.enServo.S3, זווית_רדאר2)
             מרחק = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.CENTIMETERS)
-            music.play(music.tone_playable(1000 - 6 * מרחק, music.beat(BeatFraction.SIXTEENTH)),
-                music.PlaybackMode.UNTIL_DONE)
+            if קול == 1:
+                music.play(music.tone_playable(1000 - 6 * מרחק, music.beat(BeatFraction.SIXTEENTH)),
+                    music.PlaybackMode.UNTIL_DONE)
             basic.pause(זמן_השהיה)
 basic.forever(on_forever)
 
 def on_forever2():
-    if מרחק < מרחק_גילוי_מקסימאלי:
-        music.play(music.create_sound_expression(WaveShape.SINE,
-                5000,
-                1,
-                255,
-                255,
-                500,
-                SoundExpressionEffect.VIBRATO,
-                InterpolationCurve.LINEAR),
-            music.PlaybackMode.UNTIL_DONE)
+    if מהירות > 0:
+        if כיוון == 1:
+            basic.show_leds("""
+                . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+                """)
+            SuperBit.motor_run(SuperBit.enMotors.M2, מהירות - 30)
+            SuperBit.motor_run(SuperBit.enMotors.M3, מהירות)
+        elif כיוון == -1:
+            basic.show_leds("""
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                """)
+            SuperBit.motor_run(SuperBit.enMotors.M2, מהירות)
+            SuperBit.motor_run(SuperBit.enMotors.M3, מהירות - 30)
+        elif כיוון == 0:
+            basic.show_leds("""
+                . . # . .
+                . # # # .
+                # . # . #
+                . . # . .
+                . . # . .
+                """)
+            SuperBit.motor_run(SuperBit.enMotors.M2, מהירות)
+            SuperBit.motor_run(SuperBit.enMotors.M3, מהירות)
+        else:
+            basic.show_leds("""
+                . . # . .
+                . # # # .
+                # . # . #
+                . . # . .
+                . . # . .
+                """)
+            SuperBit.motor_run(SuperBit.enMotors.M2, מהירות)
+            SuperBit.motor_run(SuperBit.enMotors.M3, מהירות)
+    elif מהירות == 0:
+        basic.show_leds("""
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            """)
+        SuperBit.motor_run(SuperBit.enMotors.M2, 0)
+        SuperBit.motor_run(SuperBit.enMotors.M3, 0)
 basic.forever(on_forever2)
+
+def on_forever3():
+    if מרחק < מרחק_גילוי_מקסימאלי:
+        if קול >= 1:
+            music.play(music.create_sound_expression(WaveShape.SINE,
+                    5000,
+                    1,
+                    255,
+                    255,
+                    500,
+                    SoundExpressionEffect.VIBRATO,
+                    InterpolationCurve.LINEAR),
+                music.PlaybackMode.UNTIL_DONE)
+        חשב_זווית_תותח(זווית_רדאר2)
+        SuperBit.servo2(SuperBit.enServo.S5, זווית_תותח)
+        SuperBit.motor_run(SuperBit.enMotors.M1, 255)
+        basic.pause(1000)
+        SuperBit.motor_run(SuperBit.enMotors.M1, 0)
+basic.forever(on_forever3)
--- main.ts
+++ main.ts
@@ -1,21 +1,39 @@
-input.onGesture(Gesture.LogoUp, function () {
-    if (הפעלת_נסיעה == 1) {
-        radio.sendValue("מהירות_גלגל_ימין", -255)
-        radio.sendValue("מהירות_גלגל_שמאל", -255)
-    } else if (הפעלת_נסיעה == 0) {
-        radio.sendValue("מהירות_גלגל_ימין", 0)
-        radio.sendValue("מהירות_גלגל_שמאל", 0)
+input.onGesture(Gesture.ScreenUp, function () {
+    while (true) {
+        radio.sendValue("מהירות", -255)
+        basic.pause(1000)
+        radio.sendValue("כיוון", 0)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
     }
 })
 input.onGesture(Gesture.TiltRight, function () {
-    if (הפעלת_נסיעה == 1) {
-        radio.sendValue("מהירות גלגל ימין", -230)
-        radio.sendValue("מהירות גלגל שמאל", -255)
-    } else if (הפעלת_נסיעה == 0) {
-        radio.sendValue("מהירות גלגל ימין", 0)
-        radio.sendValue("מהירות גלגל שמאל", 0)
-    }
+    radio.sendValue("כיוון", 1)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    music.play(music.stringPlayable("F - - - F - - - ", 200), music.PlaybackMode.UntilDone)
 })
+function חישוב_גילוי () {
+    if (מרחק_גילוי_RADAR < מרחק_גילוי_מקסימאלי) {
+        if (קול >= 1) {
+            music.play(music.createSoundExpression(WaveShape.Sine, 5000, 1, 255, 255, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+        }
+        חשב_זווית_תותח(זווית_רדאר)
+        SuperBit.Servo2(SuperBit.enServo.S5, זווית_תותח)
+        basic.pause(250)
+        בצע_יריה()
+    }
+}
 input.onButtonPressed(Button.A, function () {
     radio.sendValue("כפתור", 1)
 })
@@ -24,25 +42,42 @@ function חשב_זווית_תותח (זווית_רדאר: number) {
     זווית_תותח = זווית_תותח - 90
 }
 input.onGesture(Gesture.TiltLeft, function () {
-    if (הפעלת_נסיעה == 1) {
-        radio.sendValue("מהירות גלגל ימין", -255)
-        radio.sendValue("מהירות גלגל שמאל", -230)
-    } else if (הפעלת_נסיעה == 0) {
-        radio.sendValue("מהירות גלגל ימין", 0)
-        radio.sendValue("מהירות גלגל שמאל", 0)
-    }
+    radio.sendValue("כיוון", -1)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    music.play(music.stringPlayable("C5 - - - C5 - - - ", 200), music.PlaybackMode.UntilDone)
 })
 input.onButtonPressed(Button.AB, function () {
     if (הפעלת_נסיעה == 0) {
         הפעלת_נסיעה = 1
+        radio.sendValue("הפעלת נסיעה", 1)
     } else if (הפעלת_נסיעה == 1) {
         הפעלת_נסיעה = 0
+        מהירות = 0
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # # # # #
+            # . . . #
+            # # # # #
+            `)
         radio.sendValue("מהירות", 0)
+        radio.sendValue("הפעלת נסיעה", 0)
     }
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendValue("כפתור", 2)
 })
+function בצע_יריה () {
+    SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
+    basic.pause(1000)
+    SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
+}
 radio.onReceivedValue(function (name, value) {
     if (1 == value && "כפתור" == name) {
         זווית_רדאר = 55
@@ -58,58 +93,117 @@ radio.onReceivedValue(function (name, value) {
         } else if (רדאר_פועל == 1) {
             רדאר_פועל = 0
         }
-    } else if ("מהירות_גלגל_ימין" == name) {
-        SuperBit.MotorRun(SuperBit.enMotors.M2, value)
-    } else if ("מהירות_גלגל_שמאל" == name) {
-        SuperBit.MotorRun(SuperBit.enMotors.M3, value)
+    } else if ("מהירות" == name) {
+        מהירות = value
+    } else if ("כיוון" == name) {
+        כיוון = value
+    } else if ("הפעלת נסיעה" == name) {
+        הפעלת_נסיעה = value
     }
 })
-let מרחק_גילוי_מקסימאלי = 0
 let זווית_סריקה_מקסימאלית = 0
-let מרחק = 0
+let כיוון = 0
 let רדאר_פועל = 0
 let זמן_השהיה = 0
-let זווית_רדאר = 0
+let מהירות = 0
 let זווית_תותח = 0
+let זווית_רדאר = 0
+let קול = 0
+let מרחק_גילוי_מקסימאלי = 0
+let מרחק_גילוי_RADAR = 0
 let הפעלת_נסיעה = 0
 radio.setGroup(108)
 הפעלת_נסיעה = 0
 basic.forever(function () {
-    מרחק = 1000
+    קול = 0
+    מרחק_גילוי_RADAR = 1000
     זווית_סריקה_מקסימאלית = 110
     מרחק_גילוי_מקסימאלי = 100
     while (רדאר_פועל == 1) {
         while (זווית_רדאר < זווית_סריקה_מקסימאלית) {
             זווית_רדאר = זווית_רדאר + 10
             SuperBit.Servo2(SuperBit.enServo.S3, זווית_רדאר)
-            מרחק = sonar.ping(
+            מרחק_גילוי_RADAR = sonar.ping(
             DigitalPin.P1,
             DigitalPin.P2,
             PingUnit.Centimeters
             )
-            music.play(music.tonePlayable(1000 - 6 * מרחק, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
+            if (קול == 1) {
+                music.play(music.tonePlayable(1000 - 6 * מרחק_גילוי_RADAR, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
+            }
+            חישוב_גילוי()
             basic.pause(זמן_השהיה)
         }
         while (זווית_רדאר > 0) {
             זווית_רדאר = זווית_רדאר - 10
             SuperBit.Servo2(SuperBit.enServo.S3, זווית_רדאר)
-            מרחק = sonar.ping(
+            מרחק_גילוי_RADAR = sonar.ping(
             DigitalPin.P1,
             DigitalPin.P2,
             PingUnit.Centimeters
             )
-            music.play(music.tonePlayable(1000 - 6 * מרחק, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
+            if (קול == 1) {
+                music.play(music.tonePlayable(1000 - 6 * מרחק_גילוי_RADAR, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
+            }
+            חישוב_גילוי()
             basic.pause(זמן_השהיה)
         }
     }
 })
 basic.forever(function () {
-    if (מרחק < מרחק_גילוי_מקסימאלי) {
-        music.play(music.createSoundExpression(WaveShape.Sine, 5000, 1, 255, 255, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
-        חשב_זווית_תותח(זווית_רדאר)
-        SuperBit.Servo2(SuperBit.enServo.S5, זווית_תותח)
-        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
-        basic.pause(1000)
-        SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
+    if (הפעלת_נסיעה == 1) {
+        if (מהירות != 0) {
+            if (כיוון == 1) {
+                basic.showLeds(`
+                    . . # . .
+                    . . . # .
+                    # # # # #
+                    . . . # .
+                    . . # . .
+                    `)
+                SuperBit.MotorRun(SuperBit.enMotors.M2, מהירות + 30)
+                SuperBit.MotorRun(SuperBit.enMotors.M3, מהירות)
+            } else if (כיוון == -1) {
+                basic.showLeds(`
+                    . . # . .
+                    . # . . .
+                    # # # # #
+                    . # . . .
+                    . . # . .
+                    `)
+                SuperBit.MotorRun(SuperBit.enMotors.M2, מהירות)
+                SuperBit.MotorRun(SuperBit.enMotors.M3, מהירות + 30)
+            } else if (כיוון == 0) {
+                basic.showLeds(`
+                    . . # . .
+                    . # # # .
+                    # . # . #
+                    . . # . .
+                    . . # . .
+                    `)
+                SuperBit.MotorRun(SuperBit.enMotors.M2, מהירות)
+                SuperBit.MotorRun(SuperBit.enMotors.M3, מהירות)
+            } else {
+                basic.showLeds(`
+                    . . # . .
+                    . # # # .
+                    # . # . #
+                    . . # . .
+                    . . # . .
+                    `)
+                SuperBit.MotorRun(SuperBit.enMotors.M2, מהירות)
+                SuperBit.MotorRun(SuperBit.enMotors.M3, מהירות)
+            }
+        } else if (מהירות == 0) {
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+                `)
+            SuperBit.MotorRun(SuperBit.enMotors.M2, 0)
+            SuperBit.MotorRun(SuperBit.enMotors.M3, 0)
+        }
     }
 })
